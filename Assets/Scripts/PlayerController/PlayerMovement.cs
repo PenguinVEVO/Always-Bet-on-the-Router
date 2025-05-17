727f7c86864c6bdcd8f3b9e551085f198fd5b6ac
Mitchel <noobhunt101@gmail.com>
2025-05-18 02:13:12 +0800

    Set up slope speed modifier for player controller + working on head turn and bob

--- Assets/Scripts/PlayerController/PlayerMovement.cs
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -11,29 +11,35 @@ namespace Mitchel.PlayerController
         public float forwardSpeed = 12f;
         public float sidestepSpeed = 10f;
         [Range(1.1f, 4f)] public float sprintModifier;
-        [Range(0.1f, 1f)] public float slopeSpeedModifier;
+        [Range(0.01f, 1f)] public float slopeSpeedModifier;
         [HideInInspector] public Vector3 velocity;
 
         [Header("Gravity/Ground Settings")]
         public float gravity = -9.81f;
         public float mass = 2;
-        [Space(5)]
+        [Space(5)] 
+        [Tooltip("The minimum angle a slope needs to be at in order to slow the player's move speed")] 
+        [Range(1f, 40f)] [SerializeField] private float minSlopeMovementAngle;
         [SerializeField] private Transform groundCheck;
         [SerializeField] private float groundCheckDistance = 0.4f;
         [SerializeField] private float slopeCheckDistance = 0.5f;
         [SerializeField] private LayerMask groundMask;
-        private bool _isGrounded;
+        private bool isGrounded;
 
         [Header("Vertical Head Bob")] 
         [SerializeField] private AnimationCurve headBobCurve;
+        [SerializeField] private float cameraRollAmount;
+        [SerializeField] private float cameraRollSpeed;
         private float stepLength;
         
         private CharacterController controller;
+        private PlayerMouseLook mouseLook;
     
         // Start is called before the first frame update
         void Start()
         {
             controller = GetComponent<CharacterController>();
+            mouseLook = GetComponent<PlayerMouseLook>();
             stepLength = headBobCurve.keys[headBobCurve.length - 1].time;
         }
 
@@ -43,13 +49,17 @@ namespace Mitchel.PlayerController
             // Get forward and side axis inputs
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
+            
 
             // Calculate gravity depending on if the player is grounded and how heavy they are
-            _isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
-            if (_isGrounded && velocity.y < 0)
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+            if (isGrounded && velocity.y < 0)
             {
-                velocity.y = -2f;
+                velocity.y = -20f;
             }
+            
+            //if (isGrounded)
+                //HeadBob(x, z);
             
             // Calculate sprint speed
             float sprintMultiply = Input.GetKey(KeyCode.LeftShift) 
@@ -62,24 +72,49 @@ namespace Mitchel.PlayerController
             velocity.z = z * forwardSpeed * sprintMultiply;
             
             // Slope detection to ensure the player slides down a slope rather than "falling" off of it
+            // Also slow the player if the slope is above a minimum angle
+            // Code help by ChatGPT: https://chatgpt.com/share/6828c7a9-8744-8002-a57a-d810a79ba04a
             Vector3 horizontalMove = new Vector3(velocity.x, 0, velocity.z);
-            if (_isGrounded)
+            float slopeMultiply = 1;
+            if (isGrounded)
             {
                 Vector3 slopeNormal = GetSlopeNormal();
                 if (slopeNormal != Vector3.up)
                 {
                     horizontalMove = Vector3.ProjectOnPlane(horizontalMove, slopeNormal);
+                    float slopeAngle = Vector3.Angle(slopeNormal, Vector3.up);
+                    Debug.Log(slopeAngle);
+                    if (slopeAngle > minSlopeMovementAngle)
+                        slopeMultiply = slopeSpeedModifier;
                 }
-                else velocity.y = 0;
+                else
+                {
+                    velocity.y = 0;
+                }
             }
+            
+            
 
             // Calculate final velocity with the local transform of the player
             Vector3 move = transform.right * horizontalMove.x + 
-                           transform.up * velocity.y + 
+                           transform.up * (slopeMultiply * velocity.y) + 
                            transform.forward * horizontalMove.z;
             
             controller.Move(move * Time.deltaTime);
         }
+
+        // TODO: Set this up properly
+        /*private void HeadBob(float x, float z)
+        {
+            Quaternion currentRotation = mouseLook.playerCamera.transform.rotation;
+            Quaternion endRotation = new Quaternion(currentRotation.x,
+                                                    currentRotation.y, 
+                                                    x * cameraRollAmount, 
+                                                    currentRotation.w);
+            float rollStep = cameraRollSpeed * Time.deltaTime;
+            Quaternion roll = Quaternion.RotateTowards(currentRotation, endRotation, rollStep);
+            mouseLook.playerCamera.transform.rotation = roll;
+        }*/
         
         #region =====  UTILITY FUNCTIONS  =====
         Vector3 GetSlopeNormal()
